68305e071acd7dde704212cb9e04499ebc58d11b
MandalShivam <143705207+MandalShivam@users.noreply.github.com>
2024-01-10 06:37:21 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//console.log(null > 0);  // on comarison operator  null will be converted as 0 but for equality check
+                            // it will act as different
+//console.log(null == 0); // false
+console.log(null >= 0); // true
+
+//console.log(undefined == 0); // false
+//console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+// === 
+
+console.log("2" === 2); //checks value as well as datatype (strictly check)
+// it will give false coz data type is different === check datatype automatic converion not occur
+
