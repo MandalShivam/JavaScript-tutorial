ca754bdeb874c2fb67bacd5c8832882fae31b0b2
MandalShivam <143705207+MandalShivam@users.noreply.github.com>
2024-01-11 07:58:43 +0000

    added space

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -2,6 +2,7 @@ const name = "hitesh"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value"); -- it is not good practice now a days
+
 // always use  back tick
 //console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
@@ -39,3 +40,4 @@ console.log(str.slice(2,-2))
 
 
 // String literals (denoted by double or single quotes) and strings returned from String calls in a non-constructor context (i.e., without using the new keyword) are primitive strings. JavaScript automatically converts primitives to String objects, so that it's possible to use String object methods for primitive strings. In contexts where a method is to be invoked on a primitive string or a property lookup occurs, JavaScript will automatically wrap the string primitive and call the method or perform the property lookup.
+
