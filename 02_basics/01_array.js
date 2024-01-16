9a7c05e1df58ffe991eb91ef80cdd63679d5a567
MandalShivam <143705207+MandalShivam@users.noreply.github.com>
2024-01-16 09:24:41 +0000

    arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,63 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+//console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+//console.log(myn1);
+//console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+//console.log("C ", myArr);
+//console.log(myn2);
+
+//----------------
+
+const arr = [1,4,3,2,5,6];
+console.log(arr);
+
+let arr1 =[4,6,9,0,7,5]
+//console.log(arr1) // [4,6,9,0,7,5]
+
+let myArr1 = new Array(0,9,7,6,5,4)
+console.log(myArr1)
+
+const ar = ["shivam",'sonu',"dhhd",78,67]
+//console.log(ar)//["shivam",'sonu',"dhhd",78,67]
+//console.log(ar[-9]) // undefined
+//console.log(ar[2]) //dhhd
+
+
+let arr2 =[4,6,9,0,7,5]
+arr2.push(33)
+console.log(arr2)
+arr2.push(90)
+console.log(arr2)
+
